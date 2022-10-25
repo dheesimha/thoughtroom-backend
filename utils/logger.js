92060ef74f5817f1bf4843e7252e18eff63ec471
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2022-10-25 09:03:51 +0530

    Minor tweaks

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,11 @@
+// Extract info
 const info = (...params) => {
   if (process.env.NODE_ENV !== "test") {
     console.log(...params);
   }
 };
 
+// Print during error
 const error = (...params) => {
   if (process.env.NODE_ENV !== "test") {
     console.error(...params);
